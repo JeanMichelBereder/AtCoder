fb88521f46cd5157e86c842bd7b47a61d2f43c5a
JeanMichelBereder <jean.michel.bereder@gmail.com>
2022-03-02 10:09:40 +0900

    Added practice A solution

---
+++ welcome_to_atcoder_practiceA.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // First input (int)
+    int a;
+    cin >> a;
+    // Second input with space (int)
+    int b,c;
+    cin >> b >> c;
+    // Third input (string)
+    string s;
+    cin >> s;
+    // Output
+    cout << (a+b+c) << " " << s << endl;
+    return 0;
+}
