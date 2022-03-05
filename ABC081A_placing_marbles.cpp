287ac511173b45ac658946debf8f8142e7fc8b65
JeanMichelBereder <jean.michel.bereder@gmail.com>
2022-03-05 23:42:40 +0900

    Commit ABC081A program

---
+++ ABC081A_placing_marbles.cpp
@@ -0,0 +1,17 @@
+// Contest url : https://atcoder.jp/contests/abs/tasks/abc081_a
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    // First input (int)
+    int input, a, b, c;
+    cin >> input;
+    // Calculate product
+    a = input % 100;
+    b = input % 10;
+    c = (input - a - b ) / 100;
+    // Output
+    cout << a + b + c;
+    return 0;
+}
